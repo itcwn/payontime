c2f8251566da2a9a5b333fd5da93ec55ae8940fe
itcwn <52242108+itcwn@users.noreply.github.com>
2026-01-08 15:55:18 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,3 +1,3 @@
-export const SUPABASE_URL = "YOUR_SUPABASE_URL";
-export const SUPABASE_ANON_KEY = "YOUR_SUPABASE_ANON_KEY";
+export const SUPABASE_URL = "https://jujlyypeaqvgsnqyqzui.supabase.com";
+export const SUPABASE_ANON_KEY = "sb_publishable_ZQLXdbIJNEfJ7UnoZoz0Iw_gj2jeYYI";
 export const APP_BASE_URL = window.location.origin;
